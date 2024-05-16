8f8e3af6e462c03eba9b43fdb3b63fc41b24f0b7
hamzaanwar12 <hahamzanwar12@gmail.com>
2024-05-17 02:20:50 +0500

    First Try to Start with Copy Box Dimension

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import * as THREE from "three";
+//import SceneInit from './lib/SceneInit';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
 
-  return (
-    <>
-      <div className='w-[60%] text-white mx-auto bg-blue-500'><h1>
-        Check kar</h1></div>
-    </>
-  )
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    
+    camera.position.z = 5;
+    
+    function animate() {
+      requestAnimationFrame( animate );
+    
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+    
+      renderer.render( scene, camera );
+    }
+    
+    animate() 
+  }, []);
+  return 
+  <>
+    <div>
+      <canvas id="myThreeJsCanvas" />
+    </div>
+  </>;
 }
 
-export default App
+export default App;
