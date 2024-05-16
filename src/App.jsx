1151a2d8acb7755f93643c3a519a0e03ae674b71
hamzaanwar12 <hahamzanwar12@gmail.com>
2024-05-16 22:58:48 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-[60%] text-white mx-auto bg-blue-500'><h1>
+        Check kar</h1></div>
+    </>
+  )
+}
+
+export default App
